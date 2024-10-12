5dee888fc10e8b5b7105f467902fb23d7dcd1050
Igor Mariano <igor-mariano@outlook.com>
2024-10-12 14:46:42 -0300

    feat: Adicionando precision, recall e f1 para calcular os acertos do software

--- app/src/main/java/com/example/deteccaobatida/ConfusionMatrix.kt
+++ app/src/main/java/com/example/deteccaobatida/ConfusionMatrix.kt
@@ -19,6 +19,49 @@ class ConfusionMatrix(private val numClasses: Int) {
         }
     }
 
+    // Verifica quantos casos positivos foram classificados corretamente
+    fun getRecall(classIndex: Int): Double {
+        val truePositive = matrix[classIndex][classIndex]
+        var falseNegative = 0
+        for (i in matrix.indices) {
+            if (i != classIndex) {
+                falseNegative += matrix[classIndex][i]
+            }
+        }
+        return if (truePositive + falseNegative == 0) {
+            0.0
+        } else {
+            truePositive.toDouble() / (truePositive + falseNegative)
+        }
+    }
+
+    // verifica quantos casos classificados como positivos são realmente positivos
+    fun getPrecision(classIndex: Int): Double {
+        val truePositive = matrix[classIndex][classIndex]
+        var falsePositive = 0
+        for (i in matrix.indices) {
+            if (i != classIndex) {
+                falsePositive += matrix[i][classIndex]
+            }
+        }
+        return if (truePositive + falsePositive == 0) {
+            0.0
+        } else {
+            truePositive.toDouble() / (truePositive + falsePositive)
+        }
+    }
+
+    // Calcula o F1 Score usando 
+    fun getF1Score(classIndex: Int): Double {
+        val precision = getPrecision(classIndex)
+        val recall = getRecall(classIndex)
+        return if (precision + recall == 0.0) {
+            0.0
+        } else {
+            2 * (precision * recall) / (precision + recall)
+        }
+    }
+
     override fun toString(): String {
         val builder = StringBuilder()
         for (i in matrix.indices) {
